454681c111b9e98486ca28ed40cf3b1d003fdbea
Ramona Botezatu <rbotezatu@Lenovo.com>
2019-08-26 20:15:06 +0300

    adding managers page

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,7 +1,8 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
@@ -10,6 +11,8 @@ import { ManagersComponent } from './managers/managers.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AddFormComponent } from './add-form/add-form.component';
 import { ManagerDetailsComponent } from './manager-details/manager-details.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 
 @NgModule({
@@ -26,13 +29,17 @@ import { ManagerDetailsComponent } from './manager-details/manager-details.compo
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule 
+    ReactiveFormsModule,
+    NgxDatatableModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   //in this array we need to register all our dependecies. we register our services as providers;
   providers: [
   ],
   bootstrap: [AppComponent],
   exports: [
+    
   ]
 })
 export class AppModule { }
--- my-app/src/app/employees/employees.component.ts
+++ my-app/src/app/employees/employees.component.ts
@@ -3,13 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css']
+  styleUrls: ['./employees.component.scss']
 })
 export class EmployeesComponent implements OnInit {
+  title="angular-datatables";
 
-  constructor() { }
+
+  row=[
+    { employee: 'John Doe', project: 'LXCO', manager: 'Popescu Ionel'},
+    { employee: 'Employee1', project: 'LXCA', manager: 'Manager1'},
+    { employee: 'Vasilescu', project: 'LXCA', manager: 'Manager1'}
+  ];
+
+  columns=[
+    { prop: 'employee'},
+    { name: 'project'},
+    { name: 'manager'}
+  ];
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    //fetch: 
+    // this.fetch((data)=>{
+    //   console.log(data);
+    //   this.row=data;
+    // });
   }
+
+  // fetch(cb){
+  //   const req = new XMLHttpRequest();
+  //   req.open('GET', 'http:////swimlane.github.io/ngx-datatable/assets/data/company.json');
+
+  //   req.onload=()=> {
+  //     const data = JSON.parse(req.response);
+  //     cb(data);
+  //   };
+
+  //   req.send();
+  // }
 
 }
